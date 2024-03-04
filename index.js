cee1b15b87ea14ea1be88252cd049c1e2ba563fc
DEBARGHYA CHOWDHURY <120175307+dt1111rc@users.noreply.github.com>
2024-03-04 11:47:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(<h1>React</h1>, document.getElementById("root");
+)
+
+var 
+h1.innerHTML = "React";
+document.getElementById("root").appendChild(h1);
